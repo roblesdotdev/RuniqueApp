6d3a3a892f29d0d0f594928738e776e4f2a4d813
roblesdotdev <httpsnip@gmail.com>
2024-05-09 14:14:19 -0300

    run: display map with current location

--- run/presentation/src/main/java/com/roblesdotdev/run/presentation/active_run/ActiveRunScreen.kt
+++ run/presentation/src/main/java/com/roblesdotdev/run/presentation/active_run/ActiveRunScreen.kt
@@ -33,6 +33,7 @@ import com.roblesdotdev.core.presentation.designsystem.components.RuniqueScaffol
 import com.roblesdotdev.core.presentation.designsystem.components.RuniqueToolbar
 import com.roblesdotdev.run.presentation.R
 import com.roblesdotdev.run.presentation.active_run.components.RunDataCard
+import com.roblesdotdev.run.presentation.active_run.maps.TrackerMap
 import com.roblesdotdev.run.presentation.util.hasLocationPermission
 import com.roblesdotdev.run.presentation.util.hasNotificationPermission
 import com.roblesdotdev.run.presentation.util.shouldShowLocationPermissionRationale
@@ -140,6 +141,13 @@ fun ActiveRunScreen(
                 .fillMaxSize()
                 .background(MaterialTheme.colorScheme.surface)
         ) {
+            TrackerMap(
+                isRunFinished = state.isRunFinished,
+                currentLocation = state.currentLocation,
+                locations = state.runData.locations,
+                onSnapshot = {},
+                modifier = Modifier.fillMaxSize()
+            )
             RunDataCard(
                 elapsedTime = state.elapsedTime,
                 runData = state.runData,
