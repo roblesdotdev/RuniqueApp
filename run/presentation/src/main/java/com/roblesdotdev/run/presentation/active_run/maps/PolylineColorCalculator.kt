70a02a3595b3f2114f440852e2368fbd0b7aaaaa
roblesdotdev <httpsnip@gmail.com>
2024-05-09 15:07:12 -0300

    feat: draw polyline on map

---
+++ run/presentation/src/main/java/com/roblesdotdev/run/presentation/active_run/maps/PolylineColorCalculator.kt
@@ -0,0 +1,42 @@
+package com.roblesdotdev.run.presentation.active_run.maps
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.core.graphics.ColorUtils
+import com.roblesdotdev.core.domain.location.LocationTimestamp
+import kotlin.math.abs
+
+object PolylineColorCalculator {
+    fun locationsToColor(location1: LocationTimestamp, location2: LocationTimestamp): Color {
+        val distanceMeters = location1.location.location.distanceTo(location2.location.location)
+        val timeDiff = abs((location2.durationTimestamp - location1.durationTimestamp).inWholeSeconds)
+        val speedKmh = (distanceMeters / timeDiff) * 3.6
+
+        return interpolateColor(
+            speedKmh = speedKmh,
+            colorStart = Color.Green,
+            colorMid = Color.Yellow,
+            colorEnd = Color.Red
+        )
+    }
+
+    private fun interpolateColor(
+        speedKmh: Double,
+        colorStart: Color,
+        colorMid: Color,
+        colorEnd: Color
+    ): Color {
+        val minSpeed = 5.0
+        val maxSpeed = 20.0
+        val ratio = ((speedKmh - minSpeed) / (maxSpeed - minSpeed)).coerceIn(0.0..1.0)
+        val colorInt = if(ratio <= 0.5) {
+            val midRatio = ratio / 0.5
+            ColorUtils.blendARGB(colorStart.toArgb(), colorMid.toArgb(), midRatio.toFloat())
+        } else {
+            val midToEndRatio = (ratio - 0.5) / 0.5
+            ColorUtils.blendARGB(colorMid.toArgb(), colorEnd.toArgb(), midToEndRatio.toFloat())
+        }
+
+        return Color(colorInt)
+    }
+}
---
+++ run/presentation/src/main/java/com/roblesdotdev/run/presentation/active_run/maps/PolylineList.kt
@@ -0,0 +1,39 @@
+package com.roblesdotdev.run.presentation.active_run.maps
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import com.google.android.gms.maps.model.JointType
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.Polyline
+import com.roblesdotdev.core.domain.location.LocationTimestamp
+
+@Composable
+fun PolylineList(locations: List<List<LocationTimestamp>>) {
+    val polylineList = remember(locations) {
+        locations.map {
+            it.zipWithNext { timestamp1, timestamp2 ->
+                PolylineUi(
+                    location1 = timestamp1.location.location,
+                    location2 = timestamp2.location.location,
+                    color = PolylineColorCalculator.locationsToColor(
+                        location1 = timestamp1,
+                        location2 = timestamp2
+                    )
+                )
+            }
+        }
+    }
+
+    polylineList.forEach { polyline ->
+        polyline.forEach { polylineUi ->
+            Polyline(
+                points = listOf(
+                    LatLng(polylineUi.location1.lat, polylineUi.location1.long),
+                    LatLng(polylineUi.location2.lat, polylineUi.location2.long),
+                ),
+                color = polylineUi.color,
+                jointType = JointType.BEVEL
+            )
+        }
+    }
+}
